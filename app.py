83a2ab7e53c74ed47ba0ee0835b42b9cb415d55c
HyunggiPark <phg9587@naver.com>
2022-10-25 16:38:27 +0900

    회원가입 기능 구현 완료

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('localhost', 27017)
+db = client.krafton09
 
-## URL 별로 함수명이 같거나,
-## route('/') 등의 주h소가 같으면 안됩니다.
 
+# HTML 화면 보여주기
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+
+@app.route('/signup')
+def signup_page():
     return render_template('signup.html')
 
 
-@app.route('/singup')
-def singup():
-    return render_template('signup.html')
+# API 역할을 하는 부분
+@app.route('/api/signup', methods=['POST'])
+def signup_post():
+    email_receive = request.form['email_give']
+    pw_receive = request.form['pw_give']
+    name_receive = request.form['name_give']
+
+    user = {'email': email_receive, 'pw': pw_receive, 'name': name_receive}
+
+    # 3. mongoDB에 데이터를 넣기
+    db.users.insert_one(user)
+
+    return jsonify({'result': 'success', 'comment': "회원가입이 완료되었습니다."})
 
 
 if __name__ == '__main__':
