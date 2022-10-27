18a4c0f6c2a48ec93c229e9f319d0d14538f6292
Juhong An <juhong.ahn.dev@gmail.com>
2022-10-27 11:41:03 +0900

    도메인 불일치로 로그아웃 안됨

--- static/navbar.js
+++ static/navbar.js
@@ -1,5 +1,6 @@
 function logout() {
-  document.cookie = 'account=; expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=localhost; path=/'
+  document.cookie = 'account=; expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=king-dev.shop; path=/'
+  //document.cookie = 'account=; expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=localhost; path=/'
   alert("로그아웃 되었습니다.")
   location.href = "/";
 }
